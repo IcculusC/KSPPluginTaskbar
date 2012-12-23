7d31f5de62180a18000ec6048d5333719e38ebc9
Leath Cooper <leathcooper@gmail.com>
2012-12-23 01:32:12 -0500

    added source

---
+++ PluginTaskbar.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using UnityEngine;
+
+namespace TaskbarHook
+{
+    /**
+     *          PLUGIN TASKBAR USAGE
+     *          
+     *      This API provides you with several options for hooking your Minimize/Maximize function to the toolbar.
+     * 
+     *      This file must be included in your project, but there are no DLL dependencies.
+     *      
+     *      Method 1 will be the simplest to implement, and is generally probably the best way to go.
+     *      
+     *              Step 1: Load your icons
+     *              
+     *                  // get KSP directory
+     *                  string kspDir = KSPUtil.ApplicationRootPath.Replace("\\", "/");
+     *                  
+     *                  // intialize icons
+     *                  private static Texture2D onIcon = new Texture2D(30, 30, TextureFormat.ARGB32, false);
+     *                  private static Texture2D offIcon = new Texture2D(30, 30, TextureFormat.ARGB32, false);
+     *                  
+     *                  // load icons, change part folder/icon name!
+     *                  WWW IconOnImg = new WWW("file://" + kspDir + "Parts/YOURPARTNAMEHERE/YOURICONNAMEHERE..png");
+     *                  IconOnImg.LoadImageIntoTexture(onIcon);
+     *                  WWW IconOffImg = new WWW("file://" + kspDir + "Parts/YOURPARTNAMEHERE/YOURICONNAMEHERE.png");
+     *                  IconOffImg.LoadImageIntoTexture(offIcon);
+     *                  
+     *              Step 2:
+     *              
+     *                  Method 1
+     * 
+     *                  What you need:
+     *                  30x30 On/Off icons loaded as Texture2D
+     *                  a public void drawGUI() function containing your draw calls
+     *                  
+     *                  // check for taskbar hook
+     *                  if(!PluginTaskbar.HookToTaskbar(new Callback(drawGUI), onIcon, offIcon));
+     *                  {
+     *                      // alternative drawing method
+     *                  }
+     *                  
+     *                  Method 2
+     *                  
+     *                  What you need:
+     *                  30x30 On/Off icons loaded as Texture2D
+     *                  int[] of window ids
+     *                  Rect[] of window positions
+     *                  GUIContent[] of titles
+     *                  GUI.WindowFunction[] of void(int id) functions
+     *                  
+     *                  // check for taskbar hook
+     *                  if(!PluginTaskbar.HookToTaskbar(idArray, windowPosarray, windowTitleArray, onIcon, offIcon, windowFunctionArray));
+     *                  {
+     *                      // alternative drawing method
+     *                  }
+     * 
+     **/
+
+    class PluginTaskbar
+    {
+        private static Type[] args = { typeof(int[]), typeof(Rect[]), typeof(GUIContent[]), typeof(Texture2D), typeof(Texture2D), typeof(GUI.WindowFunction[]) };
+        private static Type[] argsd = { typeof(Callback), typeof(Texture2D), typeof(Texture2D) };
+
+        public static bool TaskbarLoaded()
+        {
+            GameObject go = null;
+            
+            if ((go = GameObject.Find("PluginTaskbarImmortal")) == null)
+                return false;
+
+            if (go.GetComponent("TaskBar") == null)
+                return false;
+
+            return true;
+        }
+
+        public static bool HookToTaskbar(Callback function, Texture2D onIcon, Texture2D offIcon)
+        {
+            if (!TaskbarLoaded())
+                return false;
+
+            GameObject go = GameObject.Find("PluginTaskbarImmortal");
+
+            object o = go.GetComponent("TaskBar");
+
+            Type t = o.GetType();
+
+            MethodInfo m = t.GetMethod("Hook", argsd);
+
+            if (m != null)
+            {
+                m.Invoke(o, new object[] { function, onIcon, offIcon });
+            }
+
+            return true;
+        }
+
+        public static bool HookToTaskbar(int[] id, Rect[] windowPos, GUIContent[] content, Texture2D onIcon, Texture2D offIcon, GUI.WindowFunction[] WindowGUI)
+        {
+            if (!TaskbarLoaded())
+                return false;
+
+            GameObject go = GameObject.Find("PluginTaskbarImmortal");
+
+            object o = go.GetComponent("TaskBar");
+
+            Type t = o.GetType();
+            
+            MethodInfo m = t.GetMethod("Hook", args);
+
+            if (m != null)
+            {
+                m.Invoke(o, new object[] { id , windowPos , content , onIcon, offIcon, WindowGUI  });
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Taskbar.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PluginTaskbar
+{
+    class TaskbarModule : PartModule 
+    {
+        public override void OnAwake()
+        {
+            Immortal.AddImmortal<TaskBar>();
+        }
+    }
+        
+    public class TaskBar : MonoBehaviour
+    {
+        private List<TaskBarHook> m_Modules = new List<TaskBarHook>();
+        private List<TaskBarDelegate> m_Delegates = new List<TaskBarDelegate>();
+        private Rect windowPos;
+        private Vector2 scrollPos = new Vector2(0, 0);
+
+        public void Start()
+        {
+            windowPos = new Rect(Screen.width - 300, 0, 200, 40);
+        }
+
+        public void window(int id)
+        {
+            GUILayout.BeginHorizontal();
+
+            RectOffset temp = GUI.skin.window.margin;
+
+            GUI.skin.window.margin.top = GUI.skin.window.margin.bottom;
+            
+            if(m_Modules.Count > 0)
+            {
+                for (int i = 0; i < m_Modules.Count; i++)
+                {
+                    TaskBarHook hook = m_Modules[i];
+
+                    if(GUILayout.Button(hook.Minimized ? hook.Icon.IconOn : hook.Icon.IconOff, GUILayout.MinWidth(30.0f), GUILayout.MinHeight(30.0f), GUILayout.MaxWidth(30.0f), GUILayout.MaxHeight(30.0f)))
+                        hook.Minimized = !hook.Minimized;  
+                }
+            }
+
+            GUI.skin.window.margin = temp;
+
+            GUILayout.EndHorizontal();
+        }
+
+        public void OnGUI()
+        {
+            if (!HighLogic.LoadedSceneIsFlight || !FlightGlobals.ready)
+                return;
+
+            GUI.skin = AssetBase.GetGUISkin("KSP window 2");
+
+            GUI.skin.label.normal.textColor = Color.white;
+            GUI.skin.label.padding = GUI.skin.button.padding;
+
+            //windowPos = GUILayout.Window(30038, windowPos, window, GUIContent.none, GUILayout.MinWidth(200.0f), GUILayout.MaxWidth(200.0f), GUILayout.MinWidth(40.0f));
+
+            if (m_Modules.Count > 0)
+            {
+                GUILayout.BeginArea(new Rect(200, 0, 200, 60));
+
+                GUILayout.BeginHorizontal();
+
+                scrollPos = GUILayout.BeginScrollView(scrollPos, false, false);
+
+                for (int i = 0; i < m_Modules.Count; i++)
+                {
+                    TaskBarHook hook = m_Modules[i];
+
+                    if (GUILayout.Button(hook.Minimized ? hook.Icon.IconOn : hook.Icon.IconOff, GUILayout.MinWidth(30.0f), GUILayout.MinHeight(30.0f), GUILayout.MaxWidth(30.0f), GUILayout.MaxHeight(30.0f)))
+                        hook.Minimized = !hook.Minimized;
+                }
+
+                GUILayout.EndScrollView();
+
+                GUILayout.EndHorizontal();  
+            }
+
+            GUILayout.EndArea();
+
+            foreach (TaskBarHook hook in m_Modules)
+            {
+                if (!hook.Minimized)
+                    hook.Draw();
+            }
+
+            foreach (TaskBarDelegate hook in m_Delegates)
+            {
+                if (!hook.Minimized)
+                    hook.Draw();
+            }
+        }
+
+        public void Hook(int[] id, Rect[] startPos, GUIContent[] content, Texture2D iconOn, Texture2D iconOff, GUI.WindowFunction[] function)
+        {
+            TaskBarHook temp = new TaskBarHook(id, startPos, content, new TaskBarIcon(iconOn, iconOff), function);
+
+            HookModule(temp);
+
+            return;
+        }
+
+        public void Hook(Callback function, Texture2D iconOn, Texture2D iconOff)
+        {
+            TaskBarDelegate temp = new TaskBarDelegate(function, new TaskBarIcon(iconOn, iconOff));
+
+            HookModule(temp);
+
+            return;
+        }
+
+        public void HookModule(TaskBarDelegate hook)
+        {
+            if (!m_Delegates.Contains(hook))
+            {
+                m_Delegates.Add(hook);
+            }
+        }
+        public void HookModule(TaskBarHook hook)
+        {
+            if (!m_Modules.Contains(hook))
+            {
+                m_Modules.Add(hook);
+            }
+
+            return;
+        }
+    }
+}
